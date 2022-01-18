542edeafbe93f616dcb884dede16d3931af8f96e
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2022-01-18 11:53:28 -0600

    Cleaned Up some Code and filled out README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Texting Bot
- This Program will text people whatever I ask it to
+
+    To Successfully use this texting bot, you MUST change:
+
+Line : Phone Number of person you are sending text to
+Line : Message
+Line : Phone Provider of the Reciever
+Line : You must change this to your Email and Password
+
+
+Note** You must activate IMAP on your google account and Allow your google account to have Less secure access(I reccomend not using your primary email)
+
+To activate IMAP use:
+https://support.google.com/mail/answer/7126229?p=BadCredentials&visit_id=637781219190590327-2642252345&rd=2#cantsignin&zippy=%2Ci-cant-sign-in-to-my-email-client%2Cstep-change-smtp-other-settings-in-your-email-client%2Cstep-check-that-imap-is-turned-on
+    
+for guidance on allowing Less secure access use:
+https://stackoverflow.com/questions/16512592/login-credentials-not-working-with-gmail-smtp
--- TextBot.py
+++ TextBot.py
@@ -9,14 +9,14 @@ def EmailAlert(number:str,
     message: str,
     provider: str,
     sender_credentials: tuple,
-    subject: str = "Sent Using Python",
+    Subject: str = "Sent Using Python",
     smtp_server = "smtp.gmail.com",
     smtp_port: int = 465
     ):
         sender_email, email_password = sender_credentials
         receiver_email = f'{number}@{PROVIDERS.get(provider).get("sms")}'
 
-        email_message = f"Subject:{subject}\nTo:{receiver_email}\n{message}"
+        email_message = f"Subject:{Subject}\nTo:{receiver_email}\n{message}"
 
         with smtplib.SMTP_SSL(
             smtp_server, smtp_port, context=ssl.create_default_context()
@@ -24,18 +24,14 @@ def EmailAlert(number:str,
             email.login(sender_email, email_password)
             email.sendmail(sender_email,receiver_email, email_message)
 def main():
-    #Chenge Number and provider to the one you are trying to send to 
-    number = "Number"
-    message = "Hello World (Sprint)"
-    provider = "provider"#Provider placed Here as a string
+    #These are all the Recievers information
+    number = "See README FILE"
+    message = f"See README FILE"
+    provider = "See README FILE"
 
+    #This is the senders info
+    sender_credentials = ("See README FILE","See README FILE")
 
-
-    #Change the Username and password to an Email and Password you want ot send this to
-    #Note** You must activate IMAP and Allow Less secure access
-    #IMAP: https://support.google.com/mail/answer/7126229?p=BadCredentials&visit_id=637781219190590327-2642252345&rd=2#cantsignin&zippy=%2Ci-cant-sign-in-to-my-email-client%2Cstep-change-smtp-other-settings-in-your-email-client%2Cstep-check-that-imap-is-turned-on
-    #Less Secure: https://stackoverflow.com/questions/16512592/login-credentials-not-working-with-gmail-smtp
-    sender_credentials = ("UserName","Password")
     EmailAlert(number,message,provider,sender_credentials)
 
 if __name__ == "__main__":
